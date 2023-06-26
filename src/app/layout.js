48f2b33fc7131f61de6420b63f5ac4a2c5270b60
Md Waseem Akram <mdwaseem0603@gmail.com>
2023-06-26 15:05:23 +0530

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Mulish } from "next/font/google";
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
